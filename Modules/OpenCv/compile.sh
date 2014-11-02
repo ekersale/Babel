c6ded130bb2b21ac066a57f1367c764cefa8b776
Napsters <sliman.desmars@epitech.eu>
2014-11-02 17:25:33 +0100

    Suppression des mauvais fichiers pour OpenCv

--- Modules/OpenCv/compile.sh
+++
@@ -1,4 +0,0 @@
-g++ -I/usr/local/include/opencv -I/usr/include/opencv/ -I/usr/local/include/opencv -I/usr/lib/ -g -o binary *.cpp 
--lopencv_imgproc -lopencv_highgui -lopencv_ml -lopencv_video 
--lopencv_calib3d -lopencv_objdetect -lopencv_contrib
--lopencv_stitching
